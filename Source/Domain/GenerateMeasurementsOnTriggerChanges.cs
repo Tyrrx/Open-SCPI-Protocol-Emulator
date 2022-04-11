819fba0776f6c8d6f6ac68af50d38cc7672734c5
David Retzlaff <david.r3tzlaff@gmail.com>
2022-04-11 14:42:13 +0200

    - Add DomainTests.csproj - Add Unit tests for TestGenerateMeasurementsOnTriggerChanges.cs - Add failing test on empty queue

--- Source/Domain/GenerateMeasurementsOnTriggerChanges.cs
+++ Source/Domain/GenerateMeasurementsOnTriggerChanges.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Reactive.Linq;
 using Domain.Interfaces;
 using Domain.UnionTypes;
@@ -12,15 +14,21 @@ namespace Domain
             measuringDevice.TriggerStateBehaviourSubject
                 .OfType<TriggerState.WaitForTrigger_>()
                 .Subscribe(_ =>
-                {
-                    var interference = measuringDevice.GeneratorQueue.Dequeue();
-                    measuringDevice.ReadingQueue.Enqueue(
-                        MeasurementValue.Double(
-                            measuringDevice.GenerateMeasurementValue(interference)
-                        )
-                    );
-                    measuringDevice.GeneratorQueue.Enqueue(interference);
-                });
+                    RotateGeneratorQueueAndProduceMeasurement(
+                        measuringDevice.GenerateMeasurementValue,
+                        measuringDevice.GeneratorQueue,
+                        measuringDevice.ReadingQueue));
+        }
+
+        public static void RotateGeneratorQueueAndProduceMeasurement(
+            Func<double, double> valueGenerator,
+            Queue<double> measuringDeviceGeneratorQueue,
+            ConcurrentQueue<MeasurementValue> measuringDeviceReadingQueue)
+        {
+            var interference = measuringDeviceGeneratorQueue.Dequeue();
+            measuringDeviceReadingQueue.Enqueue(
+                MeasurementValue.Double(valueGenerator(interference)));
+            measuringDeviceGeneratorQueue.Enqueue(interference);
         }
     }
 }
